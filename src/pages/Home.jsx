614ed928c8f84bae537eab7e59b28564e0f25d16
Rutuja Patil <130379905+Rutujapatil2411@users.noreply.github.com>
2026-02-04 19:02:33 +0530

    Home page update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import HomeSection1 from "../components/home/HomeSection1";
-import HomeSection2 from "../components/Home/homesection2";
+import HomeSection2 from "../components/home/HomeSection2";
 import TopInfoBar from "../components/Navbar/TopInfoBar";
 
 function Home() {
   return (
     <>
+      <TopInfoBar />
       <div className="min-h-screen">
         <HomeSection1 />
+        <HomeSection2 />
       </div>
-      <HomeSection2 />
+      
     </>
   );
 }
