9393af511882aa7104b9a1037ec842618bd85155
Rutuja Patil <130379905+Rutujapatil2411@users.noreply.github.com>
2026-02-05 15:30:42 +0530

    Homepage update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -231,7 +231,7 @@ const moreItems = [
                 </motion.div>
                 <div className="text-right">
                   <h1 className="text-xl font-bold bg-gradient-to-r from-blue-400 via-purple-400 to-blue-400 bg-clip-text text-transparent animate-gradient-x">
-                    Vayanta AI Labs
+                    Vyantra AI Labs
                   </h1>
                   <p className="text-xs text-gray-400 mt-0.5 mr-9">Intelligent Solutions</p>
                 </div>
