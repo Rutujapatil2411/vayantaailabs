88d55582fa513ded87c543aeb9caeb4770c9b2d5
Rutuja Patil <130379905+Rutujapatil2411@users.noreply.github.com>
2026-02-24 18:20:25 +0530

    update

--- src/components/home/HomeSection2.jsx
+++ src/components/home/HomeSection2.jsx
@@ -492,7 +492,10 @@ const HomeSection2 = () => {
             playsInline
             className="w-full h-full object-cover max-h-[600px]"
           >
-<source src="/videos/video (2).mp4" type="video/mp4" />
+<source
+        src={`${import.meta.env.BASE_URL}videos/video2.mp4`}
+        type="video/mp4"
+      />
           </video>
         </div>
 
